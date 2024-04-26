63e4a2d66b4e3c2741403552e94b018efe33ea12
Prokofiev Andrii <prokofiev.main@gmail.com>
2024-04-27 02:34:49 +0300

    StatisticsModeView coverage, button blanks for later

--- src/main/java/main/StatisticsModeView.java
+++ src/main/java/main/StatisticsModeView.java
@@ -1,5 +1,7 @@
 package main;
 
+import Entities.Store_Product;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,31 +11,32 @@ import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
-import main.Authorization;
-import main.EmployeeProfile;
-import main.HelloApplication;
-import main.MainMenu;
 import services.CategoryService;
+import services.ProductService;
 import sessionmanagement.UserInfo;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class StatisticsModeView implements Initializable {
     @FXML
     public Label employeePIB;
+
+    //UI core buttons
     @FXML
-    public Button exitAccountButton, profileButton, topCategoriesByAvgPriceButton, backButton;
+    public Button exitAccountButton, profileButton, backButton;
+
+    //BarChartMode buttons
     @FXML
-    public BarChart<String, Double> topCategoriesByAvgPriceChart;
+    public Button topProductsByPriceButton, topCategoriesByAvgPriceButton, topProductsBySoldAmountsButton, topCategoriesBySoldAmountsButton, topEmployeesByChecksButton, workerOfTheWeekButton;
+    @FXML
+    public BarChart<String, Double> barChart;
     public ImageView logo;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        employeePIB.setText(UserInfo.employeeProfile.getFullName());
     }
 
     @FXML
@@ -54,6 +57,39 @@ public class StatisticsModeView implements Initializable {
         HelloApplication.setScene(HelloApplication.mainStage, new FXMLLoader(HelloApplication.class.getResource("MainMenu.fxml")), MainMenu.WIDTH, MainMenu.HEIGHT, "Main Menu");
     }
 
+    public void showTopProductsByPrice(ActionEvent actionEvent) {
+        logo.setVisible(false);
+        XYChart.Series<String, Double> series = new XYChart.Series<>();
+        ObservableList<Store_Product> data = new ProductService().getStoreProducts();
+        Map<String, Double> data2 = new LinkedHashMap<>();
+
+        for (Store_Product sp : data){
+            data2.put(sp.getProductName(), sp.getSelling_price());
+        }
+
+        for (String key : data2.keySet()){
+            series.getData().add(new XYChart.Data<>(key, data2.get(key)));
+        }
+
+        barChart.getData().clear();
+        barChart.getData().addAll(series);
+        barChart.setVisible(true);
+    }
+
+    public void showTopProductsBySoldAmounts(ActionEvent actionEvent) {
+        logo.setVisible(false);
+        XYChart.Series<String, Double> series = new XYChart.Series<>();
+//
+//        Map<String, Double> data = new CategoryService().getCategoryAveragePrices();
+//        for (String s : data.keySet()){
+//            series.getData().add(new XYChart.Data<>(s, data.get(s)));
+//        }
+//
+        barChart.getData().clear();
+        barChart.getData().addAll(series);
+        barChart.setVisible(true);
+    }
+
     public void showTopCategoriesByAvgPrice(ActionEvent actionEvent) {
         logo.setVisible(false);
         XYChart.Series<String, Double> series = new XYChart.Series<>();
@@ -63,7 +99,52 @@ public class StatisticsModeView implements Initializable {
             series.getData().add(new XYChart.Data<>(s, data.get(s)));
         }
 
-        topCategoriesByAvgPriceChart.getData().addAll(series);
-        topCategoriesByAvgPriceChart.setVisible(true);
+        barChart.getData().clear();
+        barChart.getData().addAll(series);
+        barChart.setVisible(true);
+    }
+
+
+
+    public void showTopCategoriesBySoldAmounts(ActionEvent actionEvent) {
+        logo.setVisible(false);
+        XYChart.Series<String, Double> series = new XYChart.Series<>();
+//
+//        Map<String, Double> data = new CategoryService().getCategoryAveragePrices();
+//        for (String s : data.keySet()){
+//            series.getData().add(new XYChart.Data<>(s, data.get(s)));
+//        }
+//
+        barChart.getData().clear();
+        barChart.getData().addAll(series);
+        barChart.setVisible(true);
+    }
+
+    public void showTopEmployeesByChecksButton(ActionEvent actionEvent) {
+        logo.setVisible(false);
+        XYChart.Series<String, Double> series = new XYChart.Series<>();
+//
+//        Map<String, Double> data = new CategoryService().getCategoryAveragePrices();
+//        for (String s : data.keySet()){
+//            series.getData().add(new XYChart.Data<>(s, data.get(s)));
+//        }
+//
+        barChart.getData().clear();
+        barChart.getData().addAll(series);
+        barChart.setVisible(true);
+    }
+
+    public void showWorkerOfTheWeek(ActionEvent actionEvent) {
+        logo.setVisible(false);
+        XYChart.Series<String, Double> series = new XYChart.Series<>();
+//
+//        Map<String, Double> data = new CategoryService().getCategoryAveragePrices();
+//        for (String s : data.keySet()){
+//            series.getData().add(new XYChart.Data<>(s, data.get(s)));
+//        }
+//
+        barChart.getData().clear();
+        barChart.getData().addAll(series);
+        barChart.setVisible(true);
     }
 }
